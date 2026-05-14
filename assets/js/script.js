d3b6c19a5c76e7b6dee7333451c8a32ec0cb7574
Gustavo Pereira <gustavo.h.pereira13@aluno.senai.br>
2026-05-14 10:24:49 -0300

    ADD: Criação dos cards, os forms e os botões de alert.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,33 @@
 
+const enviar = document.getElementById('enviar_formulario');
 
+enviar.addEventListener('click', () =>{
+    const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger"
+  },
+  buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+  title: "Tem Certeza?",
+  text: "Verifique se seu endereço de Email e respostas estão corretas.",
+  icon: "info",
+  showCancelButton: true,
+  confirmButtonText: "Confirmar",
+  cancelButtonText: "Cancelar",
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) swalWithBootstrapButtons.fire({
+    title: "Enviado!",
+    text: "Seu formulário foi enviado com sucesso, aguarde nossa resposta!",
+    icon: "success"
+  });
+  else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
+    title: "Cancelado",
+    text: "Refaça o formulário.",
+    icon: "error"
+  });
+});
 
-// const swalWithBootstrapButtons = Swal.mixin({
-//   customClass: {
-//     confirmButton: "btn btn-success",
-//     cancelButton: "btn btn-danger"
-//   },
-//   buttonsStyling: false
-// });
-// swalWithBootstrapButtons.fire({
-//   title: "Tem Certeza?",
-//   text: "Verifique se seu endereço de Email e respostas estão corretas.",
-//   icon: "info",
-//   showCancelButton: true,
-//   confirmButtonText: "Sim, enviar",
-//   cancelButtonText: "Não, cancele",
-//   reverseButtons: true
-// }).then((result) => {
-//   if (result.isConfirmed) swalWithBootstrapButtons.fire({
-//     title: "Enviado!",
-//     text: "Seu formulário foi enviado com sucesso, aguarde nossa resposta!",
-//     icon: "success"
-//   });
-//   else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
-//     title: "Cancelado",
-//     text: "Refaça o formulário.",
-//     icon: "error"
-//   });
-// });
+})
