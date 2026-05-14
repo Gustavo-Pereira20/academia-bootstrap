7172ac5665328540042d83c138e98360bb980ef2
Gustavo Pereira <gustavo.h.pereira13@aluno.senai.br>
2026-05-14 11:04:20 -0300

    MODIFY: Finalização do Container Carousel e da aba de Planos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,107 @@ const enviar = document.getElementById('enviar_formulario');
 const fish = document.getElementById('plano_fish');
 const shark = document.getElementById('plano_shark');
 const mega = document.getElementById('plano_mega');
+const contato = document.getElementById('contato');
+
+
+fish.addEventListener('click', () =>{
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger"
+  },
+  buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+  title: "Fazer a Inscrição?",
+  text: "Você poderá cancelar a qualquer momento.",
+  icon: "info",
+  showCancelButton: true,
+  confirmButtonText: "Sim",
+  cancelButtonText: "Não",
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) swalWithBootstrapButtons.fire({
+    title: "Sucesso",
+    text: "Sua inscrição foi aceita.",
+    icon: "success"
+  });
+  else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
+
+    title: "Cancelado",
+    text: "Veja também nossas outras inscrições!",
+    icon: "error"
+  });
+});
+});
+
+
+shark.addEventListener('click', () =>{
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger"
+  },
+  buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+  title: "Fazer a Inscrição?",
+  text: "Você poderá cancelar a qualquer momento.",
+  icon: "info",
+  showCancelButton: true,
+  confirmButtonText: "Sim",
+  cancelButtonText: "Não",
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) swalWithBootstrapButtons.fire({
+    title: "Sucesso",
+    text: "Sua inscrição foi aceita.",
+    icon: "success"
+  });
+  else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
+
+    title: "Cancelado",
+    text: "Veja também nossas outras inscrições!",
+    icon: "error"
+  });
+});
+});
+
+mega.addEventListener('click', () =>{
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger"
+  },
+  buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+  title: "Fazer a Inscrição?",
+  text: "Você poderá cancelar a qualquer momento.",
+  icon: "info",
+  showCancelButton: true,
+  confirmButtonText: "Sim",
+  cancelButtonText: "Não",
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) swalWithBootstrapButtons.fire({
+    title: "Sucesso",
+    text: "Sua inscrição foi aceita.",
+    icon: "success"
+  });
+  else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
+
+    title: "Cancelado",
+    text: "Veja também nossas outras inscrições!",
+    icon: "error"
+  });
+});
+});
+
+
+
+
+
 
 enviar.addEventListener('click', () =>{
     const swalWithBootstrapButtons = Swal.mixin({
@@ -25,107 +126,19 @@ swalWithBootstrapButtons.fire({
     title: "Enviado!",
     text: "Seu formulário foi enviado com sucesso, aguarde nossa resposta!",
     icon: "success"
+
   });
   else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
     title: "Cancelado",
     text: "Refaça o formulário.",
     icon: "error"
   });
-});
-
-});
-
-fish.addEventListener('click', () =>{
-    const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: "btn btn-success",
-    cancelButton: "btn btn-danger"
-  },
-  buttonsStyling: false
-});
-swalWithBootstrapButtons.fire({
-  title: "Tem Certeza?",
-  text: "Verifique se seu endereço de Email e respostas estão corretas.",
-  icon: "info",
-  showCancelButton: true,
-  confirmButtonText: "Confirmar",
-  cancelButtonText: "Cancelar",
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) swalWithBootstrapButtons.fire({
-    title: "Enviado!",
-    text: "Seu formulário foi enviado com sucesso, aguarde nossa resposta!",
-    icon: "success"
-  });
-  else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
-    title: "Cancelado",
-    text: "Refaça o formulário.",
-    icon: "error"
-  });
+  contato.reset();
 });
 
 });
 
 
-shark.addEventListener('click', () =>{
-    const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: "btn btn-success",
-    cancelButton: "btn btn-danger"
-  },
-  buttonsStyling: false
-});
-swalWithBootstrapButtons.fire({
-  title: "Tem Certeza?",
-  text: "Verifique se seu endereço de Email e respostas estão corretas.",
-  icon: "info",
-  showCancelButton: true,
-  confirmButtonText: "Confirmar",
-  cancelButtonText: "Cancelar",
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) swalWithBootstrapButtons.fire({
-    title: "Enviado!",
-    text: "Seu formulário foi enviado com sucesso, aguarde nossa resposta!",
-    icon: "success"
-  });
-  else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
-    title: "Cancelado",
-    text: "Refaça o formulário.",
-    icon: "error"
-  });
-  
-
-});
 
 
-mega.addEventListener('click', () =>{
-    const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: "btn btn-success",
-    cancelButton: "btn btn-danger"
-  },
-  buttonsStyling: false
-});
-swalWithBootstrapButtons.fire({
-  title: "Tem Certeza?",
-  text: "Verifique se seu endereço de Email e respostas estão corretas.",
-  icon: "info",
-  showCancelButton: true,
-  confirmButtonText: "Confirmar",
-  cancelButtonText: "Cancelar",
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) swalWithBootstrapButtons.fire({
-    title: "Enviado!",
-    text: "Seu formulário foi enviado com sucesso, aguarde nossa resposta!",
-    icon: "success"
-  });
-  else if (result.dismiss === Swal.DismissReason.cancel) swalWithBootstrapButtons.fire({
-    title: "Cancelado",
-    text: "Refaça o formulário.",
-    icon: "error"
-  });
-});
 
-});
